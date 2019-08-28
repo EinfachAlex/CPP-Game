5019e5e60313952464ee1d10617af571f2c5270d
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2019-08-28 21:11:27 +0200

    Chrono benutzen anstatt ctime

--- Game/FPSCounter.h
+++ Game/FPSCounter.h
@@ -1,23 +1,24 @@
 #pragma once
 
-#include <ctime>
+#include <chrono>
 #include <iostream>
 
 int frames = 0;
-long int start = static_cast<long int> (std::time(0));
-long int lasttimestamp = start;
+std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+std::chrono::time_point<std::chrono::system_clock> lasttimestamp = start;
 
 void calculateFPS() {
+	std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
 	frames++;
-	long int now = static_cast<long int> (std::time(0));
-	if (now != lasttimestamp) {
+
+	long int now_seconds = now.time_since_epoch().count() / 10000000;
+	long int lasttimestamp_seconds = lasttimestamp.time_since_epoch().count() / 10000000;
+
+	if (now_seconds != lasttimestamp_seconds) {
+		std::cout << "FPS: " << (frames / (now_seconds - lasttimestamp_seconds)) << "\n";
+
 		lasttimestamp = now;
-		if ((lasttimestamp - start) < 1) {
-			lasttimestamp = start + 1;
-		}
-		std::cout << "FPS: " << (frames / (lasttimestamp - start)) << "\n";
 		frames = 0;
-		start = static_cast<long int> (std::time(0));
-		//start = std::time(0);
+		start = std::chrono::system_clock::now();
 	}
 }
