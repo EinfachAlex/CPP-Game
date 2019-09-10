aa9ab42556126f51bb1383150b74bc5917dfced9
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2019-09-10 20:08:47 +0200

    add worldblockcoordinates class

---
+++ Game/MainGame/World/Blocks/WorldBlockCoordinates.cpp
@@ -0,0 +1,14 @@
+#include "WorldBlockCoordinates.h"
+
+int x;
+int y;
+
+WorldBlockCoordinates::WorldBlockCoordinates() {
+
+}
+
+WorldBlockCoordinates::WorldBlockCoordinates(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
---
+++ Game/MainGame/World/Blocks/WorldBlockCoordinates.h
@@ -0,0 +1,11 @@
+#pragma once
+class WorldBlockCoordinates
+{
+public:
+	int x;
+	int y;
+
+	WorldBlockCoordinates();
+	WorldBlockCoordinates(int x, int y);
+};
+
