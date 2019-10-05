fdc80f36075dbd15ba842ae4a0e3eff7b4fc70c1
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2019-10-05 20:03:07 +0200

    rename block to tile

--- Game/MainGame/Overworld/Tiles/WorldTile.cpp
+++ Game/MainGame/Overworld/Tiles/WorldTile.cpp
@@ -6,8 +6,8 @@ WorldTile::WorldTile() {
 
 WorldTile::WorldTile(WorldBlockCoordinates coordinates){
 	this->coordinates = coordinates;
-	this->block = sf::RectangleShape(sf::Vector2f(16.0f, 16.0f));
-	this->block.setFillColor(sf::Color::Red);
+	this->tile = std::make_shared<sf::RectangleShape>(sf::Vector2f(16.0f, 16.0f));
+	this->tile->setFillColor(sf::Color::Red);
 }
 
 WorldTile::~WorldTile() {
